e57d7fab9d90f15e74b50f208a9adf4faeff4059
Irvin Gilwood <irvingil@bu-f1.tash.21-school.uz>
2025-11-19 15:14:10 +0500

    cleng

--- src/cycle_shift.c
+++ src/cycle_shift.c
@@ -2,10 +2,10 @@
 
 #define MAX 10
 
-int scan_single_int(int *x); // Считывание одного числа строго
-int scan_array(int *a, int n);    // Считывание массива
-void shift(int *a, int n, int c); // Сдвиг
-void output(int *a, int n);       // Вывод
+int scan_single_int(int *x);  // Считывание одного числа строго
+int scan_array(int *a, int n);     // Считывание массива
+void shift(int *a, int n, int c);  // Сдвиг
+void output(int *a, int n);        // Вывод
 
 int main() {
   int n, c;
@@ -39,10 +39,9 @@ int main() {
 // Строгое считывание 1 целого числа
 int scan_single_int(int *x) {
   char end;
-  if (scanf("%d%c", x, &end) != 2)
-    return 0; // если не прочитали два элемента
+  if (scanf("%d%c", x, &end) != 2) return 0;  // если не прочитали два элемента
   if (end != '\n' && end != ' ')
-    return 0; // если после числа не пробел или новая строка
+    return 0;  // если после числа не пробел или новая строка
   return 1;
 }
 
@@ -50,13 +49,11 @@ int scan_single_int(int *x) {
 int scan_array(int *a, int n) {
   char end;
   for (int i = 0; i < n; i++) {
-    if (scanf("%d", &a[i]) != 1)
-      return 0; // поймали букву или пустоту
+    if (scanf("%d", &a[i]) != 1) return 0;  // поймали букву или пустоту
   }
 
   // Проверяем, что после последнего числа стоит \n, а не ещё текст
-  if (scanf("%c", &end) != 1 || end != '\n')
-    return 0;
+  if (scanf("%c", &end) != 1 || end != '\n') return 0;
 
   return 1;
 }
@@ -69,17 +66,14 @@ void shift(int *a, int n, int c) {
   else
     c = n - ((-c) % n);
 
-  for (int i = 0; i < n; i++)
-    temp[i] = a[(i + c) % n];
+  for (int i = 0; i < n; i++) temp[i] = a[(i + c) % n];
 
-  for (int i = 0; i < n; i++)
-    a[i] = temp[i];
+  for (int i = 0; i < n; i++) a[i] = temp[i];
 }
 
 void output(int *a, int n) {
   for (int i = 0; i < n; i++) {
     printf("%d", a[i]);
-    if (i < n - 1)
-      printf(" ");
+    if (i < n - 1) printf(" ");
   }
 }
